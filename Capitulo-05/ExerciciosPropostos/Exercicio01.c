b86c26ca7bf2cab98c726c87b87f3a01d907bd5c
Gabriel Matos Nogueira <1563147@sga.pucminas.br>
2024-11-05 08:15:36 -0300

    ObrigadoHeitor

--- Capitulo-05/ExerciciosPropostos/Exercicio01.c
+++ Capitulo-05/ExerciciosPropostos/Exercicio01.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 int main(){
-    int vetor[6];
+    int vetor[6], contpar=0, contimp=0;
 
     for(int i=0; i<6; i++){
-        printf("Digite o valor da %d posicao: ", i);
-        scanf("%d", vetor[i]);
+        printf("Digite o valor da %d posicao: ", i+1);
+
+        scanf("%d", &vetor[i]);
+
+        if(vetor[i]%2 == 0){
+            contpar++;
+        }
+        if(vetor[i]%2 != 0){
+            contimp++;
+        }
+
     }
     for(int i=0; i<6; i++){
-        printf("O valor da posicao %d e: %d", i, vetor[i]);
+        if(vetor[i]%2 == 0){
+            printf("O numero %d e par\n", vetor[i]);
+        }
+        if(vetor[i]%2 != 0){
+            printf("O numero %d e impar\n", vetor[i]);
+        }
     }
+        printf("A quantidade de numeros impares e: %d\n", contimp);
+        printf("A quantidade de numeros par e: %d\n", contpar);
     return 0;
 }
