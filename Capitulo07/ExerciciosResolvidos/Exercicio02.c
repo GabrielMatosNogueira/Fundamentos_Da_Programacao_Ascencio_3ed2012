788fd11c618b1b59112c8f1e624702c07825fbb4
Gabriel Nogueira <gabrielmatosnogueiracompsci@gmail.com>
2024-11-11 20:19:46 -0300

    dormir

---
+++ Capitulo07/ExerciciosResolvidos/Exercicio02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    float matriz[10][3], menorNota, maiorNota;
+
+    printf("Digite a nota dos alunos: ");
+    for(int i=0; i<10; i++){
+        for(int j=0; j<3; j++){
+            printf("Digite a nota do aluno %d pertencente a prova %d", i+1, j+1);
+            scanf("%d", &matriz[10][3]);
+        }
+    }
+
+}
